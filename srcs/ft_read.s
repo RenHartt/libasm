c09636caaa324003492b5e8b4fdfa13e2fad7a98
RenHartt <baptiste.goron@gmail.com>
2024-09-18 19:49:42 +0200

    add ft_read

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -6,7 +6,7 @@
 ;    By: bgoron <bgoron@42angouleme.fr>             +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2024/09/18 19:30:34 by bgoron            #+#    #+#              ;
-;    Updated: 2024/09/18 19:32:20 by bgoron           ###   ########.fr        ;
+;    Updated: 2024/09/18 19:36:36 by bgoron           ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -16,4 +16,15 @@ section	.text
 global	ft_read
 
 ft_read:
-
+	mov		rax, 0
+	syscall
+	cmp		rax, 0
+	jl		.fail
+	ret
+.fail:
+	neg		rax
+	mov		rdi, rax
+	call	__errno_location
+	mov		[rax], rdi
+	mov		rax, -1
+	ret
--- tests/test_read.c
+++ tests/test_read.c
@@ -6,17 +6,27 @@
 /*   By: bgoron <bgoron@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 19:29:36 by bgoron            #+#    #+#             */
-/*   Updated: 2024/09/18 19:32:43 by bgoron           ###   ########.fr       */
+/*   Updated: 2024/09/18 19:47:48 by bgoron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 ssize_t read(int fd, void *buf, size_t count);
 
 int main(void)
 {
+	int fd = open("test.txt", O_RDONLY);
+	char *buffer = malloc(1);
+
+	ssize_t size = read(fd, buffer, 1);
+	printf("Char readed : %s\n", buffer);
+	printf("Errno : %s\n", strerror(errno));
+
 	return (0);
 }
